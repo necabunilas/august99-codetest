e63e72e356e9b235af08451234a287b55f8cf2a0
Nicole Eric Cabunilas <necabunilas@gmail.com>
2020-08-28 10:14:45 +0800

    add lazyload

--- src/Spacex.js
+++ src/Spacex.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Info from './Info';
+import LazyLoad from 'react-lazyload';
 
 function Spacex() {
     const [error, setError] = useState(null);
@@ -21,16 +22,24 @@ function Spacex() {
             )
     }, [])
 
-    function showItem(item){
-        return(
-            <Info item={item} />
-        )
+    const Loading = () =>(
+        <div className="inner">
+            <h3>Loading . .</h3>
+        </div>
+    )
+
+    function showItem(item, index){
+        return (
+			<LazyLoad height={200} offset={100} once={true} key={index} placeholder={<Loading />}>
+				<Info item={item} />
+			</LazyLoad>
+		);
     }
 
     if (error) {
         return <div className="container">Error: {error}</div>;
     } else if (!isLoaded) {
-        return <div className="container">Loading...</div>;
+        return <div className="container">{<h1>Loading...</h1>}</div>;
     } else {
         return (
             <div className="container">
